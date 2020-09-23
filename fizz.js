022a04a423daa859da584be806ce8afb33533f85
Leesel Fraser <leeselfraser08@gmail.com>
2020-09-22 20:08:21 -0400

    classwork

--- fizz.js
+++ fizz.js
@@ -5,13 +5,20 @@
 
 //To test your code, set a value for x, and console.log it. Try different values
 
+let result;
+const x = 3
 
+if (x % 3 === 0 && x % 5 === 0) {
+  result = "fizzbuzz";
+} else if (x % 3 === 0) {
+  result = "fizz";
+} else if (x % 5 === 0) {
+  result = "buzz";
+} else {
+ result = x;
+}
 
-
-
-
-
-
+console.log(result)
 
 
 
@@ -20,3 +27,19 @@
 //Taking what we learned from the conditionals practice with fizzbuzz, We are going to Loop through every number from 1 to max
 //applying those same exact rules to each number and, before ending the loop,
 //printing out the result to the console via console.log(result). A For Loop would work
+
+
+let result;
+const max = 100;
+for (let x = 1; x <= max; x += 1) {
+  if (x % 3 === 0 && x % 5 === 0) {
+    result = "fizzbuzz";
+  } else if (x % 3 === 0) {
+    result = "fizz";
+  } else if (x % 5 === 0) {
+    result = "buzz";
+  } else {
+    result = x;
+  }
+}
+console.log(result)
