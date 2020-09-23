f6206699d7e6b3d8fd9ae53f8e0b102156e7fec3
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2020-09-22 20:09:07 -0400

    Completed

--- fizz.js
+++ fizz.js
@@ -4,19 +4,35 @@
 //If x is not evenly divisible by either 3 or 5 (for example 8), set result to x
 
 //To test your code, set a value for x, and console.log it. Try different values
-
-
-
-
-
-
-
-
-
-
-
+let result;
+const x = 15;
+if (x % 3 === 0 && x % 5 === 0) {
+  result = "FizzBuzz";
+} else if (x % 3 === 0) {
+  result = "Fizz";
+} else if (x % 5 === 0) {
+  result = "Buzz";
+} else {
+  return x;
+}
+console.log(result);
 
 //part 2
 //Taking what we learned from the conditionals practice with fizzbuzz, We are going to Loop through every number from 1 to max
 //applying those same exact rules to each number and, before ending the loop,
 //printing out the result to the console via console.log(result). A For Loop would work
+
+let result;
+const max = 100;
+for (let x = 0; x <= max; 1++) {
+  if (x % 3 === 0 && x % 5 === 0) {
+    result = "FizzBuzz";
+  } else if (x % 3 === 0) {
+    result = "Fizz";
+  } else if (x % 5 === 0) {
+    result = "Buzz";
+  } else {
+    return x;
+  }
+}
+console.log(result);
